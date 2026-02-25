b2e2d19d7232a06c0f33a55e45b84ee72a34cf24
DevBarke <111465018+DevBarke@users.noreply.github.com>
2026-02-25 11:44:17 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,17 @@
+-- loading for my cute russian spy 🤓
+-- loading for my cute russian spy 🤓
+-- loading for my cute russian spy 🤓
+-- loading for my cute russian spy 🤓
+-- loading for my cute russian spy 🤓
+-- loading for my cute russian spy 🤓
+-- loading for my cute russian spy 🤓
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DevBarke/VoidLoader/refs/heads/main/main.lua"))()
+
+-- loading for my cute russian spy 🤓
+-- loading for my cute russian spy 🤓
+-- loading for my cute russian spy 🤓
+-- loading for my cute russian spy 🤓
+-- loading for my cute russian spy 🤓
+-- loading for my cute russian spy 🤓
+-- loading for my cute russian spy 🤓
