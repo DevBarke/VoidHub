21e585e354085f8a46c7aa4243edf851dfee6539
DevBarke <111465018+DevBarke@users.noreply.github.com>
2026-03-02 17:57:35 +0300

    Delete main.lua

--- main.lua
+++
@@ -1,4 +0,0 @@
--- ples dont skid me brooooooo i will take a screenshot of you!! 😡😡😡
--- we friends right
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DevBarke/VoidLoader/refs/heads/main/main.lua"))()
